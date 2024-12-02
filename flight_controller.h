de84f1aa2f594ea2a2466ed4555364dc419b5d32
otienomaurice1 <134706372+otienomaurice1@users.noreply.github.com>
2024-12-01 23:30:59 -0500

    Add files via upload

--- flight_controller.h
+++ flight_controller.h
@@ -1,6 +1,7 @@
 #ifndef FLIGHT_CONTROLLER_H
 #define FLIGHT_CONTROLLER_H
-#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+
 //This section dec;lares the pins to which the pico delivers pwm 
 //signaals to the ESC
 
@@ -8,19 +9,24 @@
 #define FRONT_RIGHT
 #define BACK_LEFT
 #define BACK_RIGHT 
+
 //GYROSCOPE AND ACCELEROMETER CONNECTIONS
-#define IMU_SDA
-#define IMU_SCLK
+#define IMU_SDA  20
+#define IMU_SCLK 21
+
 //UV_SENSOR CONNECTION
 #define UVS_TRIG
 #define UVS_ECHO
+
 //BLUETOOTH CONNECTOION
 #define BLUETOOTH_RX
 #define BLUETOOTH_TX
 #define BLUETOOTH_STATE
+
 //BAROMETER CONNECTION
-#define BAROMETER_SDA
-#define BAROMETER_SCLK
+#define BAROMETER_SDA 26
+#define BAROMETER_SCLK 27
+
 //RECEIVER CONNECTION
 #define RECEIVER_UART0
 #define RECEIVER_UART1 
